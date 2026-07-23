049cf7acd2a9fc75f9cb3d02c6a406661a6aa928
schoenstatt13 <schoenstatt04@gmail.com>
2026-07-22 23:49:17 -0600

    Oculta credenciales de base de datos usando variables de entorno

--- SARA.py
+++ SARA.py
@@ -1,12 +1,18 @@
+import os
 import mysql.connector
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+# Esto carga las variables secretas del archivo .env
+load_dotenv()
 
 def get_connection():
+    # Ahora Python buscará la contraseña en el archivo oculto
     conexion = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="K1mpata7a",
-        database="sara"
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
     return conexion
 
